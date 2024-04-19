35e4c61d3fbca598361fca79a1e1a3fbedafb3b9
DavideC0 <davidecalcagni.dc@gmail.com>
2024-04-19 16:50:30 +0200

    creato il file lezione4 e risolto il primo problema

---
+++ Lezione4/lezione4.py
@@ -0,0 +1,22 @@
+"""
+    Questa funzione prende in input un dizionario
+    (e.s. d = {"ciao": 2, "hello": 3}) e restituisce
+    un nuovo dizionario fatto così:
+    d1 = {"ciao": 2/5, "hello": 3/5}) dove 5 è l somma
+    dei valori di d, ossia 2 + 3 = 5
+
+"""
+def rewrite_dict(d: dict[str,int]) -> dict[str, int]:
+    somma: int = 0
+    d1: dict = d.copy()
+    for key in d:
+        somma += d[key]
+    
+    for key in d1:
+        d1[key] = d1[key] / somma
+    
+    return d1
+
+d: dict = {"ciao": 2, "hello": 3}
+d1: dict = rewrite_dict(d)
+print(f"Il dizionario normale è {d}, quello trasformato è {d1}")
