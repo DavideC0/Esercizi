23feacee1dd177f9822861d4ee0accf90e291541
DavideC0 <davidecalcagni.dc@gmail.com>
2024-04-23 11:05:03 +0200

    aggiunto esercizio 4-2

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -2,3 +2,8 @@
 pizza: list = ["margherita", "diavola", "marinara"]
 for i in range(len(pizza)):
     print(f"Il tipo di pizza che preferisco è: {pizza[i]}")
+
+#4-2
+animali: list = ["gatto", "cane", "coniglio"]
+for i in range(len(animali)):
+    print(f"Il {animali[i]} è un ottimo animale")
