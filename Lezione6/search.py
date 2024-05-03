c49c3096ea38dcb4921b1a4236734809e1928495
DavideC0 <davidecalcagni.dc@gmail.com>
2024-05-03 19:01:40 +0200

    aggiunta funzione per visitare un albero

--- Lezione6/search.py
+++ Lezione6/search.py
@@ -25,3 +25,14 @@ def __binary_search_recursive(array: list[int], x: int, low: int, high: int) -> 
         return __binary_search_recursive(array, x, mid + 1, high)
     else:
         return __binary_search_recursive(array, x, low, mid - 1)
+    
+def visit_tree(tree: dict[int,list[int]], node: int):
+    print(node)
+    left_child, right_child = tree.get(node, [None,None])
+    if left_child:
+        visit_tree(tree, left_child)
+    if right_child:
+        visit_tree(tree, right_child)
+
+tree = {4:[3,5], 3:[2,None], 5:[4.5,6], 2:[None,None], 4.5:[None,None], 6:[None,None]}
+visit_tree(tree,4)
