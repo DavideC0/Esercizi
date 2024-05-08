f410bb56ed84609cb2283fc6d60076d2fa4ee1e1
DavideC0 <davidecalcagni.dc@gmail.com>
2024-05-08 10:51:03 +0200

    aggiunto metodo str al menu e messa la descrizione per ogni cibo

--- Lezione6/esercizio4.py
+++ Lezione6/esercizio4.py
@@ -1,7 +1,8 @@
 class Food:
-    def __init__(self, name: str, price: float) -> None:
+    def __init__(self, name: str, price: float, description: str) -> None:
         self.name: str = name
         self.price: float = price
+        self.description = description
     
     def get_name(self) -> str:
         return self.name
@@ -10,11 +11,17 @@ class Food:
         return self.price
     
     def __str__(self) -> str:
-        return f"nome = {self.name} prezzo = {self.price}"
+        return f"nome = {self.name} prezzo = {self.price} descrizione = {self.description}"
     
 class Menu:
     def __init__(self) -> None:
         self.list_food: list[Food] = []
+
+    def __str__(self) -> str:
+        repr: str = ""
+        for food in self.list_food:
+            repr += food.__str__() + "\n"
+        return repr
 
     def add_food(self, food):
         self.list_food.append(food)
@@ -32,9 +39,9 @@ class Menu:
     def get_avg_price(self):
         return sum(self.get_prices()) / len(self.list_food)
 
-pizza = Food("Pizza", 10)
-pasta = Food("Pasta", 7.90)
-zuppa = Food("Zuppa", 20.50)
+pizza = Food("Pizza", 10, "wurstel e patatine")
+pasta = Food("Pasta", 7.90, "pasta cruda")
+zuppa = Food("Zuppa", 20.50, "la good zuppa")
 
 menu = Menu()
 
@@ -44,3 +51,10 @@ menu.add_food(zuppa)
 
 print(f"Lista prezzi: {menu.get_prices()}")
 print(f"Media dei prezzi: {menu.get_avg_price()}")
+print(menu)
+
+menu.remove_food(zuppa)
+print("Dopo eliminazione zuppa:")
+print(f"Lista prezzi: {menu.get_prices()}")
+print(f"Media dei prezzi: {menu.get_avg_price()}")
+print(menu)
