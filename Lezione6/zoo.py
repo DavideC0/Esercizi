d89708070f97cee5780188326db4fb497090d34b
Davide Calcagni <159887680+DavideC0@users.noreply.github.com>
2024-05-10 16:50:25 +0200

    sistemato e testato il metodo di aggiunta animali

--- Lezione6/zoo.py
+++ Lezione6/zoo.py
@@ -12,7 +12,14 @@ class Animal:
         return self.height * self.width
     
     def __str__(self) -> str:
-        return f"nome = {self.name} specie = {self.species}"
+        return (f"Name: {self.name} "
+                f"Species: {self.species} "
+                f"Age: {self.age} "
+                f"Height: {self.height} "
+                f"Width: {self.width} "
+                f"Preferred Habitat: {self.preferred_habitat} "
+                f"Health: {self.health}")
+
 
 class Fence:
     def __init__(self, area: float, temperature: float, habitat: str) -> None:
@@ -23,12 +30,17 @@ class Fence:
 
     def add(self, animal: Animal) -> None:
         self.lista_animali.append(animal)
+        self.area -= animal.calcolo_area()
 
     def __str__(self) -> str:
         repr: str = ""
         for animal in self.lista_animali:
             repr += animal.__str__() + "\n"
+        repr += f"area = {self.area} temperatura = {self.temperature} habitat = {self.habitat}"
         return repr
+    
+    def print_area(self) -> str:
+        return self.area
 
 class ZooKeeper: 
     def __init__(self, nome: str, cognome: str, id: int) -> None: 
@@ -49,9 +61,25 @@ class Zoo:
 lorenzo = ZooKeeper("Lorenzo", "Maggi", 1234)
 
 recinto = Fence(100,25,"Continental")
-
-lupo = Animal("giovanni","blb", 10, 5,5,"Continental")
-
-lorenzo.add_animal(lupo,recinto)
+recinto_vuoto = Fence(100,25,"dinosauri")
 
 print(recinto)
+
+lupo = Animal("giovanni","blb", 10, 5, 5,"Continental")
+cavallo = Animal('pietro','palle', 20, 10, 5, 'Continental')
+gatto = Animal('adrian','savona', 20, 10, 10, 'dinosauri')
+cane = Animal('nicola','ltimat-piderma', 20, 25, 1, 'Continental')
+
+lorenzo.add_animal(lupo,recinto)
+print(f"recinto 1 {recinto.print_area()}") #75 primo
+lorenzo.add_animal(cavallo,recinto)
+print(f"recinto 1 {recinto.print_area()}") #25 primo
+lorenzo.add_animal(gatto,recinto)
+print(f"recinto 1 {recinto.print_area()}") #25 non entra primo
+lorenzo.add_animal(cane,recinto)
+print(f"recinto 1 {recinto.print_area()}") #0 primo
+print(f"recinto 2 {recinto_vuoto.print_area()}") #100 secondo
+lorenzo.add_animal(gatto,recinto_vuoto)
+print(f"recinto 2 {recinto_vuoto.print_area()}") #0 secondo
+lorenzo.add_animal(lupo,recinto_vuoto)
+print(f"recinto 2 {recinto_vuoto.print_area()}") #0 non entra secondo
