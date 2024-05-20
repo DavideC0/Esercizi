d9f1e0264e9efe696a52c8ce44a75a62d71bb8c8
Davide Calcagni <159887680+DavideC0@users.noreply.github.com>
2024-05-20 18:54:54 +0200

    aggiunto esercizio 9-3

--- Lezione6/eserciziCasa.py
+++ Lezione6/eserciziCasa.py
@@ -13,3 +13,23 @@ ristorante = Restaurant("la primavera di vladimiro", "pizzeria")
 print(f"Nome: {ristorante.nome} tipo di cucina: {ristorante.tipo_cucina}")
 print(ristorante.open_restaurant())
 print(ristorante.describe_restaurant())
+
+#9-3 Users
+class Users:
+    def __init__(self, nome: str, cognome: str, residenza: str, età: int) -> None:
+        self.nome = nome
+        self.cognome = cognome
+        self.residenza = residenza
+        self.età = età
+    
+    def describe_user(self) -> str:
+        return f"nome = {self.nome} cognome = {self.cognome} residenza = {self.residenza} età = {self.età}"
+    
+    def greet_user(self) -> str:
+        return f"benvenuto {self.cognome} {self.nome}, cosa desideri fare?"
+davide = Users("Davide", "Calcagni", "Roma", 19)
+maffo = Users("Andrea", "Maffei", "Castel Giubileo", 20)
+print(davide.describe_user())
+print(davide.greet_user())
+print(maffo.describe_user())
+print(maffo.greet_user())
