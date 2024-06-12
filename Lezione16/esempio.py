a8feb670e2523bb5ad549b3421cdaf5995507028
Nio4124 <antoniozottola41204@gmail.com>
2024-06-12 17:57:16 +0200

    conte le righe nei file di test

--- Lezione16/esempio.py
+++ Lezione16/esempio.py
@@ -1,14 +1,22 @@
-reader = open("esempio.txt")
+# reader = open("esempio.txt")
 
-try:
-    print(reader.readline())
-    print(f"Sono nella try")
-    raise Exception(f"Eccezione")
+# try:
+#     print(reader.readline())
+#     print(f"Sono nella try")
+#     raise Exception(f"Eccezione")
 
-except Exception:
-    print(f"Sono nella exept")
+# except Exception:
+#     print(f"Sono nella exept")
     
-finally:
-    print(reader)
-    reader.close()
-    print(f"Sono nella finaly")
+# finally:
+#     print(reader)
+#     reader.close()
+#     print(f"Sono nella finally")
+
+with open("esempio.txt") as reader:
+    line = reader.readline()
+    line_counter = 0
+    while line != '':
+        print(f"{line} - number: {line_counter}")
+        line = reader.readline()
+        line_counter += 1
