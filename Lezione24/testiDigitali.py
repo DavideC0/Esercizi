e5fa9815d85c5699c33a2e2f75215fce0fddc146
Nio4124 <antoniozottola41204@gmail.com>
2024-07-05 11:36:54 +0200

    create classi documento e email

---
+++ Lezione24/testiDigitali.py
@@ -0,0 +1,40 @@
+class Documento:
+    def __init__(self, testo: str) -> None:
+        self.testo: str = testo
+    
+    def getText(self) -> str:
+        return self.testo
+    
+    def setText(self, testo: str) -> None:
+        self.testo = testo
+    
+    def isInText(self, parola: str) -> bool:
+        if parola in self.testo:
+            return True
+        return False
+    
+class Email(Documento):
+    def __init__(self, testo: str, mittente: str, destinatario: str, titolo: str) -> None:
+        super().__init__(testo)
+        self.mittente: str = mittente
+        self.destinatario: str = destinatario
+        self.titolo: str = titolo
+    
+    def get_mittente(self) -> str:
+        return self.mittente
+
+    def set_mittente(self, mittente: str) -> None:
+        self.mittente = mittente
+
+    def get_destinatario(self) -> str:
+        return self.destinatario
+
+    def set_destinatario(self, destinatario: str) -> None:
+        self.destinatario = destinatario
+
+    def get_titolo(self) -> str:
+        return self.titolo
+
+    def set_titolo(self, titolo: str) -> None:
+        self.titolo = titolo
+        
