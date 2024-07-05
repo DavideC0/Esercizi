9fd217011d05805589a4b9ddc82c03b514473fdf
Nio4124 <antoniozottola41204@gmail.com>
2024-07-05 13:44:59 +0200

    creata classe file

--- Lezione24/testiDigitali.py
+++ Lezione24/testiDigitali.py
@@ -1,3 +1,5 @@
+import os
+
 class Documento:
     def __init__(self, testo: str) -> None:
         self.testo: str = testo
@@ -44,3 +46,32 @@ class Email(Documento):
     def writeToFile(self, directory: str) -> None:
         with open(directory, "a") as appender:
             appender.write(self.getText())
+
+class File(Documento):
+    def __init__(self, testo: str, nomePercorso: str) -> None:
+        super().__init__(testo)
+        self.nome_percorso: str = nomePercorso
+        if not os.path.isdir(nomePercorso):
+            self.createDirectory(nomePercorso)
+        else:
+            print('Directory already exits')
+
+    def createDirectory(self, nomePercorso: str):
+        os.mkdir(nomePercorso)
+        self.createFile(nomePercorso)
+
+    def createFile(self, nomePercorso: str) -> str:
+        with open(f"{nomePercorso}/document.txt", "a") as reader:
+            testo = "Questo e' il contenuto del file: \n"
+            reader.write(testo)
+            reader.write(self.testo)
+        return "document.txt"
+    
+    def leggiTestoDaFile(self) -> str:
+        with open(f"{self.nome_percorso}/document.txt")  as reader:
+            file = reader.read()
+        return file
+    
+
+giorgio: File = File("Ciao Giorgio, come stai?", "esempiomdycr4f")
+print(giorgio.leggiTestoDaFile( ))
