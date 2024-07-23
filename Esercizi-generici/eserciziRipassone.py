050e9d078535bd2e1ae8161c5edc7047ffb46e01
Nio4124 <antoniozottola41204@gmail.com>
2024-07-23 18:00:48 +0200

    banca

--- Esercizi-generici/eserciziRipassone.py
+++ Esercizi-generici/eserciziRipassone.py
@@ -7,3 +7,28 @@ def lista_a_dizionario(tuples: list[tuple]) -> dict[str:list[int]]:
             dictionary[tuples[i][0]] = [tuples[i][1]]
     return dictionary
 
+class Account:
+    def __init__(self, id) -> None:
+        self.account_id = id
+        self.saldo = 0
+        
+    def deposit(self, amount):
+        self.saldo += amount
+        
+    def get_balance(self):
+        return self.saldo
+
+class Bank:
+    def __init__(self) -> None:
+        self.accounts: dict[str:Account] = {}
+        
+    def create_account(self, account_id):
+        account = Account(account_id)
+        self.accounts[account_id] = account
+        return account
+    
+    def deposit(self, account_id, amount):
+        self.accounts[account_id].deposit(amount)
+        
+    def get_balance(self, account_id):
+        return self.accounts[account_id].get_balance()   
