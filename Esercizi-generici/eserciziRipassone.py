90139d8bd391a18315848ce478f452bde57abc7c
Nio4124 <antoniozottola41204@gmail.com>
2024-07-23 18:33:51 +0200

    veicoli, auto e moto

--- Esercizi-generici/eserciziRipassone.py
+++ Esercizi-generici/eserciziRipassone.py
@@ -31,4 +31,29 @@ class Bank:
         self.accounts[account_id].deposit(amount)
         
     def get_balance(self, account_id):
-        return self.accounts[account_id].get_balance()   
+        return self.accounts[account_id].get_balance()  
+    
+class Veicolo:
+    def __init__(self, marca: str, modello: str, anno: int) -> None:
+        self.marca: str = marca
+        self.modello: str = modello
+        self.anno: int = anno
+        
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}")
+            
+class Auto(Veicolo):
+    def __init__(self, marca: str, modello: str, anno: int, numero_porte: int) -> None:
+        super().__init__(marca, modello, anno)
+        self.numero_porte: int = numero_porte
+        
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Numero di porte: {self.numero_porte}")
+    
+class Moto(Veicolo):
+    def __init__(self, marca: str, modello: str, anno: int, tipo: str) -> None:
+        super().__init__(marca, modello, anno)
+        self.tipo: str = tipo
+        
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Tipo: {self.tipo}")
