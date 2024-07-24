84c64d63353f51747a2dcabe7589f831ef705bb4
Nio4124 <antoniozottola41204@gmail.com>
2024-07-24 17:27:45 +0200

    check combination

--- Esercizi-generici/eserciziRipassone.py
+++ Esercizi-generici/eserciziRipassone.py
@@ -196,4 +196,8 @@ def filtra_e_mappa(prodotti: dict[str:float]) -> dict[str:float]:
             dizionario[key] = value
     return dizionario
 
-print(filtra_e_mappa({'Penna': 15.0, 'Zaino': 50.0, 'Quaderno': 22.0}))
+def check_combination(conditionA: bool, conditionB: bool, conditionC: bool) -> str:
+    if conditionA or conditionB and conditionC:
+        return "Operazione permessa"
+    else:
+        return "Operazione negata"
