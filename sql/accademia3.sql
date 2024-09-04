7dc97d5d1b213a687bf5da24aa1dad47b9990d17
Nio4124 <antoniozottola41204@gmail.com>
2024-09-04 16:34:30 +0200

    sistemati commenti

--- sql/accademia3.sql
+++ sql/accademia3.sql
@@ -1,28 +1,19 @@
--- Definizione del dominio 'Strutturato'
 CREATE TYPE Strutturato AS ENUM ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
 
--- Definizione del dominio 'LavoroProgetto'
 CREATE TYPE LavoroProgetto AS ENUM ('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
 
--- Definizione del dominio 'LavoroNonProgettuale'
 CREATE TYPE LavoroNonProgettuale AS ENUM ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro Accademico', 'Altro');
 
--- Definizione del dominio 'CausaAssenza'
 CREATE TYPE CausaAssenza AS ENUM ('Chiusura Universitaria', 'Maternita', 'Malattia');
 
--- Definizione del dominio 'PosInteger'
 CREATE DOMAIN PosInteger AS INTEGER CHECK (VALUE >= 0);
 
--- Definizione del dominio 'StringaM'
 CREATE DOMAIN StringaM AS VARCHAR(100);
 
--- Definizione del dominio 'NumeroOre'
 CREATE DOMAIN NumeroOre AS INTEGER CHECK (VALUE BETWEEN 0 AND 8);
 
--- Definizione del dominio 'Denaro'
 CREATE DOMAIN Denaro AS REAL CHECK (VALUE >= 0);
 
--- Tabella 'Persona'
 CREATE TABLE Persona (
     id PosInteger PRIMARY KEY,
     nome StringaM NOT NULL,
@@ -31,30 +22,27 @@ CREATE TABLE Persona (
     stipendio Denaro NOT NULL
 );
 
--- Tabella 'Progetto'
 CREATE TABLE Progetto (
     id PosInteger PRIMARY KEY,
-    nome StringaM NOT NULL UNIQUE, -- VincoloDB.1: nome è una chiave alternativa
+    nome StringaM NOT NULL UNIQUE, -- VincoloDB.1
     inizio DATE NOT NULL,
     fine DATE NOT NULL,
     budget Denaro NOT NULL,
-    CHECK (inizio < fine) -- VincoloDB.2: inizio deve essere minore di fine
+    CHECK (inizio < fine) -- VincoloDB.2
 );
 
--- Tabella 'WP'
 CREATE TABLE WP (
     progetto PosInteger NOT NULL,
     id PosInteger NOT NULL,
     nome StringaM NOT NULL,
     inizio DATE NOT NULL,
     fine DATE NOT NULL,
-    PRIMARY KEY (progetto, id), -- Combinazione di progetto e id come chiave primaria
-    UNIQUE (progetto, nome), -- VincoloDB.4: altra chiave
-    CHECK (inizio < fine), -- VincoloDB.3: inizio deve essere minore di fine
-    FOREIGN KEY (progetto) REFERENCES Progetto(id) -- VincoloDB.5: foreign key
+    PRIMARY KEY (progetto, id),
+    UNIQUE (progetto, nome), -- VincoloDB.4
+    CHECK (inizio < fine), -- VincoloDB.3
+    FOREIGN KEY (progetto) REFERENCES Progetto(id) -- VincoloDB.5
 );
 
--- Tabella 'AttivitaProgetto'
 CREATE TABLE AttivitaProgetto (
     id PosInteger PRIMARY KEY,
     persona PosInteger NOT NULL,
@@ -63,26 +51,24 @@ CREATE TABLE AttivitaProgetto (
     giorno DATE NOT NULL,
     tipo LavoroProgetto NOT NULL,
     oreDurata NumeroOre NOT NULL,
-    FOREIGN KEY (persona) REFERENCES Persona(id), -- VincoloDB.6: foreign key
-    FOREIGN KEY (progetto, wp) REFERENCES WP(progetto, id) -- VincoloDB.7: foreign key
+    FOREIGN KEY (persona) REFERENCES Persona(id), -- VincoloDB.6
+    FOREIGN KEY (progetto, wp) REFERENCES WP(progetto, id) -- VincoloDB.7
 );
 
--- Tabella 'AttivitaNonProgettuale'
 CREATE TABLE AttivitaNonProgettuale (
     id PosInteger PRIMARY KEY,
     persona PosInteger NOT NULL,
     tipo LavoroNonProgettuale NOT NULL,
     giorno DATE NOT NULL,
     oreDurata NumeroOre NOT NULL,
-    FOREIGN KEY (persona) REFERENCES Persona(id) -- VincoloDB.8: foreign key
+    FOREIGN KEY (persona) REFERENCES Persona(id) -- VincoloDB.8
 );
 
--- Tabella 'Assenza'
 CREATE TABLE Assenza (
     id PosInteger PRIMARY KEY,
     persona PosInteger NOT NULL,
     tipo CausaAssenza NOT NULL,
     giorno DATE NOT NULL,
-    UNIQUE (persona, giorno), -- VincoloDB.9: combinazione unica di persona e giorno
-    FOREIGN KEY (persona) REFERENCES Persona(id) -- VincoloDB.10: foreign key
+    UNIQUE (persona, giorno), -- VincoloDB.9
+    FOREIGN KEY (persona) REFERENCES Persona(id) -- VincoloDB.10
 );
