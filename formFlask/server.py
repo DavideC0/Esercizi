da07f1886b416aad0b433d79d873b8164ff30662
Nio4124 <antoniozottola41204@gmail.com>
2024-09-19 16:38:58 +0200

    creato form con flask

--- formFlask/server.py
+++ formFlask/server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 api = Flask("__name__")
+utenti = [["mario", "password1", "m", "0"], ["gianni", "password2", "m", "0"]]
 
 
 @api.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@api.route('/ciao', methods=['GET'])
-def index_buongiorno():
-    return render_template('ciao.html')
+@api.route('/visualizza', methods=['GET'])
+def visualizza():
+    nome = request.args.get('nome')
+    cognome = request.args.get('cognome')
+    return render_template('visualizza.html', nome=nome, cognome=cognome)
 
 api.run(host="0.0.0.0", port=8085)
