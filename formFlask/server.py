e106074a5acab1b6569413cb6c711ccd21e75565
Nio4124 <antoniozottola41204@gmail.com>
2024-09-19 18:23:14 +0200

    aggiunte immagini e registrazione

--- formFlask/server.py
+++ formFlask/server.py
@@ -11,7 +11,22 @@ def index():
 @api.route('/visualizza', methods=['GET'])
 def visualizza():
     nome = request.args.get('nome')
-    cognome = request.args.get('cognome')
-    return render_template('visualizza.html', nome=nome, cognome=cognome)
+    password = request.args.get('password')
+    for i in range(len(utenti)):
+        if utenti[i][0] == nome and utenti[i][1] == password:
+            return render_template('visualizza.html', nome=utenti[i][0], sesso=utenti[i][2], numero=utenti[i][3])    
+    return render_template('errore.html')
+
+@api.route('/registra', methods=['GET'])
+def registra():
+    return render_template('registra.html')
+
+@api.route('/registrazione', methods=['GET'])
+def registrazione():
+    nome = request.args.get('nome')
+    password = request.args.get('password')
+    sesso = request.args.get('sesso')
+    utenti.append([nome,password,sesso,0])
+    return render_template("index.html")
 
 api.run(host="0.0.0.0", port=8085)
